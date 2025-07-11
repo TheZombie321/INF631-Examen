215b82bd5282497f848b62cd080e95b9a19f14ab
TheZombie321 <jhonnyunochoque@gmail.com>
2025-07-11 10:11:42 -0400

    examen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Inicio';
-import DetalleEstudiante from './pages/DetalleEstudiante';
 import { FormProvider } from './context/FormContext';
 
 const App = () => (
@@ -10,7 +8,6 @@ const App = () => (
     <Router>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/detalle/:id" element={<DetalleEstudiante />} />
       </Routes>
     </Router>
   </FormProvider>
